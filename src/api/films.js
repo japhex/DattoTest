17d115d104f8f8fd1f650dd7a2e23a0401d7047e
japhex <j@japhex.net>
2020-09-09 11:53:50 +0100

    Getting rid of old api directory

--- src/api/films.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from 'react-query'
-import axios from 'axios'
-
-export const getFilms = () => {
-  return useQuery('films', async () => {
-    const { data } = await axios.get('https://ghibliapi.herokuapp.com/films')
-    return data
-  })
-}
